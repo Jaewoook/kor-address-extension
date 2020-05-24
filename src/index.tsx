354004dbd1cd2c04f0fed0fdff80a42055ee0313
Jaewook Ahn <ajw4586@gmail.com>
2020-05-24 14:25:52 +0900

    Fix inject env var logic

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import { render } from "react-dom";
 import { App } from "./App";
 import { initializeSettings, getRuntime } from "./utils";
 
+window.__ENV__ = {
+    NODE_ENV: process.env.REACT_APP_ENV as string,
+};
+
 if (getRuntime() === "extension") {
     chrome.runtime.onInstalled.addListener((details) => {
         if (details.reason === "install") {
