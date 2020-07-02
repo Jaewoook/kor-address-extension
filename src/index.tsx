cb81484edb3a6dede44e924ce45b0ad17763e2a6
Jaewook Ahn <ajw4586@gmail.com>
2020-07-02 20:34:28 +0900

    Add Sentry module to track uncaught errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import * as Sentry from "@sentry/react";
 import { render } from "react-dom";
 import { App } from "./App";
-import { getRuntime } from "./utils";
+import { getRuntime, isProduction } from "./utils";
 import { AddressManager } from "./AddressManager";
 import { SettingsManager, Settings, DEFAULT_SETTINGS } from "./SettingsManager";
 
 window.__ENV__ = {
     NODE_ENV: process.env.REACT_APP_ENV as string,
 };
+
+if (isProduction()) {
+    Sentry.init({ dsn: "https://6b96accd47ff467da8394a51da93d909@o415139.ingest.sentry.io/5305794" });
+}
 
 let settings = null;
 
