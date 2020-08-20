1a9e0d083a21002b62e8401865e856ac7deb4d49
Jaewook Ahn <ajw4586@gmail.com>
2020-08-20 18:25:53 +0900

    Add ReactGA to use google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
+import ReactGA from "react-ga";
 import { App } from "./App";
-import { getRuntime } from "./utils";
+import { getRuntime, getEnv } from "./utils";
 import { AddressManager } from "./AddressManager";
 import { SettingsManager, Settings, DEFAULT_SETTINGS } from "./SettingsManager";
 
 window.__ENV__ = {
     NODE_ENV: process.env.REACT_APP_ENV as string,
 };
+
+//  initialize google analytics
+ReactGA.initialize("UA-108816190-2", {
+    debug: getEnv() !== "production",
+});
+ReactGA.pageview(window.location.pathname);
 
 let settings = null;
 
