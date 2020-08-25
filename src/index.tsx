e91f7bcef99c29cf194f9643fa474b2a8c5e4490
Jaewook Ahn <ajw4586@gmail.com>
2020-08-26 02:04:48 +0900

    Improve: ignore analytics tracking in development

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { render } from "react-dom";
 import { App } from "./App";
-import { getRuntime } from "./utils";
+import { getRuntime, getEnv } from "./utils";
 import { AddressManager } from "./AddressManager";
 import { SettingsManager, Settings, DEFAULT_SETTINGS } from "./SettingsManager";
 
+//  inject NODE_ENV variable into window object
 window.__ENV__ = {
     NODE_ENV: process.env.REACT_APP_ENV as string,
 };
+
+//  initialize google analytics only in production
+if (getEnv() === "production") {
+    window.ga("create", "UA-108816190-2", "auto");
+    window.ga("set", "checkProtocolTask", null);
+    window.ga("send", "pageview", "/");
+} else {
+    console.info("Google Analytics disabled because runtime does not running in production.");
+    window.ga = function() {};
+}
 
 let settings = null;
 
