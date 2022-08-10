8ea8f61c460b3c6c5bea55dedee1f05ed3dd4814
Jaewook Ahn <dev.jaewook@gmail.com>
2022-08-10 21:47:12 +0900

    Add Layout components
    - write Content and Footer components

---
+++ src/components/Layout.tsx
@@ -0,0 +1,34 @@
+/**
+ * External modules
+ */
+import styled from "styled-components";
+import { Layout } from "antd";
+
+export const Header = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 1;
+    background-color: #f0f2f5;
+`;
+
+export const Content = styled(Layout.Content)`
+    margin-top: 124px;
+    background-color: #fafafa;
+`;
+
+export const Footer = styled(Layout.Footer)`
+    display: flex;
+    padding: 0 0 16px 0;
+    justify-content: center;
+    align-items: center;
+    background-color: #fafafa;
+
+    & * {
+        color: rgba(0, 0, 0, 0.3) !important;
+    }
+    & > div > * {
+        cursor: pointer;
+    }
+`;
