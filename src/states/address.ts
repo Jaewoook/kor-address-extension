59dbd93f41c75f412868cbe2f454b3943e0b62e5
Jaewook Ahn <dev.jaewook@gmail.com>
2024-06-23 01:58:31 +0900

    feat(storage): support state data synchronization with storage

--- src/states/address.ts
+++ src/states/address.ts
@@ -1,8 +1,22 @@
 import { atom } from "recoil";
 
+import { getRecentAddressList, setRecentAddressList } from "@/shared/storage";
 import type { AddressData } from "@/shared/models/address";
 
 export const addressListState = atom<AddressData[]>({
   key: "address-list",
   default: [],
+  effects: [({ trigger, setSelf, onSet }) => {
+    if (trigger === "get") {
+      getRecentAddressList()?.then((recentAddressList) => {
+        if (recentAddressList) {
+          setSelf(recentAddressList);
+        }
+      });
+    }
+
+    onSet((newAddressList) => {
+      setRecentAddressList(newAddressList);
+    });
+  }],
 });
