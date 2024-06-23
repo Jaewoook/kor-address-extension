99cee4e46594a9d710dc4d4cbe3f7a06249ff06c
Jaewook Ahn <dev.jaewook@gmail.com>
2024-06-23 22:02:06 +0900

    fix(storage): change webextension polyfill not to use
    - fix storage get logic
    - add prettier config
    - add prettier eslint integration
    - bump up version to 1.2.0

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     "eslint:recommended",
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "prettier",
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   globals: {
