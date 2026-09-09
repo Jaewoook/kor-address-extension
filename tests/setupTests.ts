862473cb09a0abe5a0216752eda47b1fb20f4c48
Jaewook Ahn <dev.jaewook@gmail.com>
2026-09-09 15:18:58 +0900

    chore: upgrade react and antd to latest major versions
    - react/react-dom 18.3.1 -> 19.2.8
    - antd 5.29.3 -> 6.6.3
    - @types/react 18.3.31 -> 19.2.18, @types/react-dom 18.3.7 -> 19.2.7
    - polyfill ResizeObserver in test setup (antd 6's Tooltip requires it, jsdom doesn't provide it)

--- tests/setupTests.ts
+++ tests/setupTests.ts
@@ -3,6 +3,15 @@ import { afterEach } from "vitest";
 
 import "@testing-library/jest-dom/vitest";
 
+// jsdom does not implement ResizeObserver; antd's Tooltip/Popup positioning
+// (rc-resize-observer) requires it to be present, even as a no-op.
+class ResizeObserverStub {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+globalThis.ResizeObserver ??= ResizeObserverStub;
+
 afterEach(() => {
   cleanup();
 });
